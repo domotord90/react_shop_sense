a93c50eaea5a3414d8e7ec025822f7fd749ab010
domotord90 <jazzmaster@freemail.com>
2019-11-16 14:07:42 +0100

    Updates

---
+++ precache-manifest.0ceab242fcb4784f49f99c51c54795d0.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737458c49041c863561104f434391bbd",
+    "url": "/react_shop_sense/index.html"
+  },
+  {
+    "revision": "1fefd02aa5b90588b02b",
+    "url": "/react_shop_sense/static/css/main.e746ac5a.chunk.css"
+  },
+  {
+    "revision": "ad08f1fa38285c29bfcb",
+    "url": "/react_shop_sense/static/js/2.199ffa62.chunk.js"
+  },
+  {
+    "revision": "1fefd02aa5b90588b02b",
+    "url": "/react_shop_sense/static/js/main.7ad8a456.chunk.js"
+  },
+  {
+    "revision": "20a3c9c78ab6f2f432b1",
+    "url": "/react_shop_sense/static/js/runtime-main.a2dcd95a.js"
+  },
+  {
+    "revision": "368b391cb371dbb9e7e975e5a60ea9e0",
+    "url": "/react_shop_sense/static/media/accessories.368b391c.jpg"
+  },
+  {
+    "revision": "465c713980c246a4f51b03e2890ce15e",
+    "url": "/react_shop_sense/static/media/all.465c7139.jpg"
+  },
+  {
+    "revision": "40fa5b915e99d0a5798f19bb57a1acf9",
+    "url": "/react_shop_sense/static/media/beauty.40fa5b91.jpg"
+  },
+  {
+    "revision": "dea1fb00e90975812289c1b05764b5c0",
+    "url": "/react_shop_sense/static/media/classic-eyeglasses.dea1fb00.jpg"
+  },
+  {
+    "revision": "c598136c137b0c9576033f79271cfad4",
+    "url": "/react_shop_sense/static/media/coach-perfume.c598136c.jpg"
+  },
+  {
+    "revision": "a131db26822b7e75628e43c94b615baa",
+    "url": "/react_shop_sense/static/media/dualshock-controller.a131db26.jpg"
+  },
+  {
+    "revision": "6bda14524787080e8e825d48cab4e15f",
+    "url": "/react_shop_sense/static/media/gaming.6bda1452.jpg"
+  },
+  {
+    "revision": "63dabb55657997f2abb317c60fc3c449",
+    "url": "/react_shop_sense/static/media/gifts.63dabb55.jpg"
+  },
+  {
+    "revision": "785c7a0a34f5e194d4f28911bb58c5ba",
+    "url": "/react_shop_sense/static/media/glasses-home.785c7a0a.jpg"
+  },
+  {
+    "revision": "f10f7f585db0d0d23ebfea0388ca05cf",
+    "url": "/react_shop_sense/static/media/headset-home.f10f7f58.jpg"
+  },
+  {
+    "revision": "f675e552d520bb7b28c383f872c18e39",
+    "url": "/react_shop_sense/static/media/map.f675e552.jpg"
+  },
+  {
+    "revision": "34028898e1c6b977b1c95602710a18f7",
+    "url": "/react_shop_sense/static/media/nintendo-ds.34028898.jpg"
+  },
+  {
+    "revision": "97e772280a8f55726367001696b7c4ca",
+    "url": "/react_shop_sense/static/media/online-shopping.97e77228.jpg"
+  },
+  {
+    "revision": "7a6bce01f041e1412abc4a67cc912773",
+    "url": "/react_shop_sense/static/media/pentax-bridge-camera.7a6bce01.jpg"
+  },
+  {
+    "revision": "134f92e3363b16885c8e2dc919a5dcb4",
+    "url": "/react_shop_sense/static/media/phone.134f92e3.jpg"
+  },
+  {
+    "revision": "abd275fa9888043bfb44f7dd5ff823a6",
+    "url": "/react_shop_sense/static/media/pink-lipstick.abd275fa.jpg"
+  },
+  {
+    "revision": "768c94eebbce28183dc68eeacc505ec7",
+    "url": "/react_shop_sense/static/media/shopping.768c94ee.jpg"
+  },
+  {
+    "revision": "b285c4f8cca6555a78df6edbcdc71ba4",
+    "url": "/react_shop_sense/static/media/technology.b285c4f8.jpg"
+  },
+  {
+    "revision": "d5a4d3ebfb2c7b2627faf0e940219663",
+    "url": "/react_shop_sense/static/media/watch-home.d5a4d3eb.jpg"
+  },
+  {
+    "revision": "889cc36b1e3e70c5abdc6c4f82b2cfe3",
+    "url": "/react_shop_sense/static/media/xbox360.889cc36b.jpg"
+  }
+]);
